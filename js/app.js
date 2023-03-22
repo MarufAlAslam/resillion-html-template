757cddbdcaae3967e7d8d67812c23dd96bb61773
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-03-22 11:48:29 +0600

    solution slider done

--- js/app.js
+++ js/app.js
@@ -33,3 +33,38 @@ $(".logo-slider").slick({
     },
   ],
 });
+$(".solution-slider").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<img src="img/prev.png" class="img-fluid prev" alt="">',
+  nextArrow: '<img src="img/next.png" class="img-fluid next" alt="">',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
